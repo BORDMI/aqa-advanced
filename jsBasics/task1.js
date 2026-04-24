7bdf7d97bfd8a12bd0a97f3b2ce1922f8687ada5
dmytro.borysenko <dmybor@gmail.com>
2026-04-24 14:52:24 +0300

    Add 4 homework files

---
+++ jsBasics/task1.js
@@ -0,0 +1,11 @@
+const str = "Just a string";
+const num = 42;
+const bool = true;
+const nothing = null;
+let undef;
+
+console.log(str);
+console.log(num);
+console.log(bool);
+console.log(nothing);
+console.log(undef);
---
+++ jsBasics/task4.js
@@ -0,0 +1,16 @@
+// 4.1
+const circleRadius = 4;
+const circleArea = Math.PI * circleRadius ** 2;
+console.log(parseFloat(circleArea.toFixed(2)));
+
+// 4.2
+const length = 10;
+const width = 5;
+const rectangleArea = parseFloat((length * width).toFixed(2));
+console.log(rectangleArea);
+
+// 4.3
+const cylinderRadius = 4;
+const height = 9;
+const cylinderVolume = parseFloat((Math.PI * cylinderRadius ** 2 * height).toFixed(2));
+console.log(cylinderVolume);
