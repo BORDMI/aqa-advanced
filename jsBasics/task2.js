4f8d06c507ea0dae48108b9b08477d1538cae79c
BORDMI <139070643+BORDMI@users.noreply.github.com>
2026-04-28 08:24:19 +0300

    Feat js primitives
    * Add index.js file and add chalk dependency
    
    * Add 4 homework files

---
+++ jsBasics/task2.js
@@ -0,0 +1,8 @@
+const firstName = "Anna";
+const secondName = "Ivan";
+
+const greetingMessage = "Hello, " + firstName + " and " + secondName + "!";
+console.log(greetingMessage);
+
+const greetingTemplate = `Hello, ${firstName} and ${secondName}!`;
+console.log(greetingTemplate);
