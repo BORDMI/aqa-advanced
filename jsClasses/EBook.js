13dff0859a998b35b3d32abcda3490af456401f9
dmytro.borysenko <dmybor@gmail.com>
2026-05-18 09:09:43 +0300

    Add jsClasses homework

---
+++ jsClasses/EBook.js
@@ -0,0 +1,34 @@
+import Book from './Book.js';
+
+class EBook extends Book {
+  #fileFormat;
+
+  constructor(title, author, year, fileFormat) {
+    super(title, author, year);
+    this.fileFormat = fileFormat;
+  }
+
+  get fileFormat() {
+    return this.#fileFormat;
+  }
+
+  set fileFormat(value) {
+    const allowed = ['PDF', 'EPUB', 'MOBI', 'FB2', 'TXT'];
+    if (typeof value !== 'string' || !allowed.includes(value.toUpperCase())) 
+      throw new Error(`File format must be one of: ${allowed.join(', ')}`);
+    
+    this.#fileFormat = value.toUpperCase();
+  }
+
+  printInfo() {
+    super.printInfo();
+    console.log(`File format: ${this.#fileFormat}`);
+  }
+
+  static fromBook(book, fileFormat) {
+    if (!(book instanceof Book))   throw new Error('First argument must be an instance of Book');
+    return new EBook(book.title, book.author, book.year, fileFormat);
+  }
+}
+
+export default EBook;
